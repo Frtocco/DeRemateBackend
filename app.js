d2c562cce5906d005e7cb3ff9598cf6f42da47a9
Francisco <franchutocco@gmail.com>
2025-04-03 20:27:40 -0300

    Se volvio a un modelo http ya que los SSL autofirmados no eran soportados por android

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-import express from 'express';
-import https from 'https';
-import fs from 'fs';
-import { userRouter } from './routes/users.js';
+import express, { json } from 'express' // require -> commonJS
+import { userRouter} from './routes/users.js'
 
-const app = express();
+
+
+const app = express()
 app.use(express.json());
-app.disable('x-powered-by');
+app.disable('x-powered-by')
 
 app.use('/users', userRouter);
 
-const PORT = process.env.PORT ?? 1234;
+const PORT = process.env.PORT ?? 1234
 
-// Cargar los certificados
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
-};
-
-// Crear el servidor HTTPS
-https.createServer(options, app).listen(PORT, () => {
-  console.log(`Server listening on https://localhost:${PORT}`);
-});
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
