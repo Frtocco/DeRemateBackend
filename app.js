6c31ac161f0fba436e4b9e765232c2063fc861fe
cristianvictorkim <cristianvkim@gmail.com>
2025-04-22 11:38:53 -0300

    BE muestra todas las peticiones a la API

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express, { json } from 'express' // require -> commonJS
 import { userRouter} from './routes/users.js'
 import { orderRouter} from './routes/orders.js'
+import morgan from 'morgan'
 
 
 const app = express()
 app.use(express.json());
 app.disable('x-powered-by')
+app.use(morgan('dev'))
 
 app.use('/users', userRouter);
 app.use('/orders', orderRouter);
--- mailer.js
+++ mailer.js
@@ -1,23 +1,24 @@
 import nodemailer from 'nodemailer'
 
 export const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'sandbox.smtp.mailtrap.io',
+  port: 2525,
   auth: {
-    user: 'deremate764@gmail.com',
-    pass: 'uadeinformatica'
+    user: '0885e930a19835',
+    pass: 'd7973dc3e33bd4fb5ae7e6ecd84e4cb8'
   }
 });
 
 export const sendResetEmail = async (to, token) => {
-  const resetLink = `https://29c4-170-239-171-153.ngrok-free.app/reset-password?token=${token}`
+  const resetLink = `http://localhost:1234/reset-password?token=${token}`
 
 
   const mailOptions = {
-    from: 'deremate764@gmail.com',
-    to,
-    subject: 'Recuperación de contraseña - DeRemate',
-    text: `Hola! Para restablecer tu contraseña, hacé click en este enlace: ${resetLink}`,
-    html: `<p>Hola!</p><p>Para restablecer tu contraseña, hacé click en el siguiente enlace:</p><a href="${resetLink}">${resetLink}</a><p>Si no lo solicitaste, ignorá este mensaje.</p>`
+      from: 'no-reply@deremate.com',
+      to,
+      subject: 'Recuperación de contraseña - DeRemate',
+      text: `Hola! Para restablecer tu contraseña, hacé click en este enlace: ${resetLink}`,
+      html: `<p>Hola!</p><p>Para restablecer tu contraseña, hacé click en el siguiente enlace:</p><a href="${resetLink}">${resetLink}</a><p>Si no lo solicitaste, ignorá este mensaje.</p>`
   }
 
   await transporter.sendMail(mailOptions)
