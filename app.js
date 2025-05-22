ab2da3689c05ce67d55b7fcc6c12f564e520f860
Pedro <pedro.16011@gmail.com>
2025-05-22 17:58:39 -0300

    Instalacion y configuracion de CORS. para consultas entre localhost.

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-import express, { json } from 'express' // require -> commonJS
-import { userRouter} from './routes/users.js'
-import { orderRouter} from './routes/orders.js'
-
+import express from 'express' // require -> commonJS
+import { userRouter } from './routes/users.js'
+import { orderRouter } from './routes/orders.js'
+import { corsMiddleware } from './middlewares/cors.js'
 
 const app = express()
-app.use(express.json());
+app.use(corsMiddleware())
+app.use(express.json())
 app.disable('x-powered-by')
 
-app.use('/users', userRouter);
-app.use('/orders', orderRouter);
+app.use('/users', userRouter)
+app.use('/orders', orderRouter)
 
 const PORT = process.env.PORT ?? 1234
 
--- routes/orders.js
+++ routes/orders.js
@@ -1,29 +1,29 @@
-import { Router } from 'express';
-import { OrderModel } from '../models/order.js';
+import { Router } from 'express'
+import { OrderModel } from '../models/order.js'
 
-export const orderRouter = Router();
+export const orderRouter = Router()
 
-orderRouter.get('/', async (req,res) => {
-    const orders = await OrderModel.getAll()
-    res.json(orders)
+orderRouter.get('/', async (req, res) => {
+  const orders = await OrderModel.getAll()
+  res.json(orders)
 })
 
-orderRouter.get('/pendings', async (req,res) => {
-    const pendingOrders = await OrderModel.getPendings()
-    res.json(pendingOrders)
+orderRouter.get('/pendings', async (req, res) => {
+  const pendingOrders = await OrderModel.getPendings()
+  res.json(pendingOrders)
 })
 
-orderRouter.get('/history', async(req,res)=>{
-    const { riderId } = req.query;
-    console.log("Consulta de historial para este rider id: ",riderId);
-    const orders = await OrderModel.getOrdersByRider(riderId)
-    res.json(orders)
+orderRouter.get('/history', async (req, res) => {
+  const { riderId } = req.query
+  console.log('Consulta de historial para este rider id: ', riderId)
+  const orders = await OrderModel.getOrdersByRider(riderId)
+  res.json(orders)
 })
 
-orderRouter.put('/:orderId', async(req, res) =>{
-    const { orderId } = req.params;
-    const { status, riderId } = req.body;
+orderRouter.put('/:orderId', async (req, res) => {
+  const { orderId } = req.params
+  const { status, riderId } = req.body
 
-    const order = await OrderModel.chengeStatus(orderId, status,riderId)
-    return res.json(order)
+  const order = await OrderModel.chengeStatus(orderId, status, riderId)
+  return res.json(order)
 })
